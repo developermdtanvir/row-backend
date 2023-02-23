e74d3894b265c173a4e5e91d4386a0e849065ed5
devmdtanvir <officialtanvir@hotmail.com>
2023-02-23 15:13:36 +0600

    Stream file is addeded

--- stream.js
+++ stream.js
@@ -1,7 +1,34 @@
-const fs = require("fs");
+const http = require("http");
 
-const ourReadStream = fs.createReadStream(`${__dirname}/demo.txt`, `utf8`);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write(`<html><head><title></title></head></html>`);
+    res.write(
+      `<body><form method="post" action="/process"><input name="message" /></form></body>`
+    );
+    res.end();
+  } else if (req.url === "/process" && req.method === "POST") {
+    let body = [];
 
-ourReadStream.on("data", (data) => {
-  console.log(data);
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      console.log("strime finished");
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+    });
+    res.write("Thanks Submitting ");
+    res.end();
+  }
 });
+
+server.listen(3000);
+
+// const fs = require("fs");
+
+// const ourReadStream = fs.createReadStream(`${__dirname}/demo.txt`, `utf8`);
+
+// ourReadStream.on("data", (data) => {
+//   console.log(data);
+// });
